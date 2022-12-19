7b2a0a91d0720f46cf6e8c04a59bd23678e13230
Zaniar Manochehri <49729648+ZaniarManochehri@users.noreply.github.com>
2022-12-19 16:04:56 +0330

    chatItem in progress

--- src/components/sidebar/chat-item/index.js
+++ src/components/sidebar/chat-item/index.js
@@ -1,18 +1,21 @@
 import styles from "./ChatItem.module.css";
 
-const ChatItem = ({ item }) => {
+const ChatItem = ({ item, selected, onClick }) => {
   const lastChat =
     item.lastChat.length > 30
       ? item.lastChat.substring(0, 30) + "..."
       : item.lastChat;
   return (
-    <div className={styles.chatItem}>
+    <div
+      className={`${styles.chatItem} ${selected && styles.selected}`}
+      onClick={onClick}
+    >
       <div className={styles.imageContainer}>
         <img src={item.avatar} alt="user-avatar" className={styles.img} />
       </div>
       <div className={styles.content}>
         <div className={styles.topSection}>
-          <span>{item.name}</span>
+          <span className={styles.name}>{item.name}</span>
           <span>{item.date}</span>
         </div>
         <span>{lastChat}</span>
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useRef, useEffect } from "react";
 
 //component
 import styles from "./Sidebar.module.css";
@@ -7,41 +7,63 @@ import Menu from "./slot/menu";
 import ChatItem from "./chat-item";
 
 const Sidebar = () => {
-    const [searchValue, setSearchValue] = useState("");
-    const chats = [
-        {
-            name: "Telegram",
-            lastChat:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            date: 'Doc 18',
-            avatar: '/telegram.png',
-        },
-        {
-            name: "Telegram1",
-            lastChat:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            date: 'Doc 17',
-            avatar: "/logo512.png",
-        },
-];
-    return (
-        <div className={styles.sidebar}>
-            <div className={styles.searchboxContainer}>
-                <Menu />
-                <Searchbox
-                    placeholder="Search"
-                    width="100%"
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                />
-            </div>
-            <div className={styles.chatListContainer}>
-                {chats.map((item, index) => (
-                    <Fragment key={index}><ChatItem item={item}/></Fragment>
-                ))}
-            </div>
-        </div>
-    );
+  const searchSectionRef = useRef(null);
+
+  const [chats, setChats] = useState()
+  const [searchValue, setSearchValue] = useState("");
+  const [searchSectionHeight, setSearchSectionHeight] = useState(0);
+  const [clickedItem, setClickedItem] = useState();
+
+  const randomString = (length) => {
+    let result           = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+
+  useEffect(() => {
+    setSearchSectionHeight(searchSectionRef.current.clientHeight + 25);
+
+    const array = []
+    for (let i = 0; i < 50; i++) {
+      array.push({
+        id: `@${randomString(6)}`,
+        name: `Telegram${i}`,
+        lastChat:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        date: "Doc 17",
+        avatar: i % 2 === 0 ? "/telegram.png" : i % 3 === 0 ? "image.jpeg" : "/logo512.png",
+      });
+    }
+    setChats(array)
+  }, []);
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.searchboxContainer} ref={searchSectionRef}>
+        <Menu />
+        <Searchbox
+          placeholder="Search"
+          width="100%"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+      <div
+        className={styles.chatListContainer}
+        style={{ height: `calc(100% - ${searchSectionHeight}px)` }}
+      >
+        {chats?.map((item, index) => (
+          <Fragment key={index}>
+            <ChatItem item={item} selected={item.id == clickedItem?.id} onClick={() => setClickedItem(item)}/>
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
